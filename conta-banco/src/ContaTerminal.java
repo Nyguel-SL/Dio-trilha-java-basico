271664271e5479ae879a2ffd4f5b25ce6e4811a6
Nyguel-SL <nygueloliveira@gmail.com>
2025-07-01 21:25:23 -0300

    Update ContaTerminal.java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+    public static void  main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Informe sua numero de sua agência:");
+        int NumberAgence = scanner.nextInt();
+        System.out.println("Informe sua agencia:");
+        var agence = scanner .next();
+        System.out.println("Informe seu nome:");
+        var name = scanner.next();
+        System.out.println("Informe seu saldo:");
+        float Saldo = scanner .nextFloat();
+        System.out.printf("olá %s, obrigaddo por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo é %s \n", name, agence,NumberAgence, Saldo);
+    }    
+}   
